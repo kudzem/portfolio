61a899038527e9c351e15d16921cd2acb5e00856
Alexander Kokrov <alexander.kokrov@orioninc.com>
2025-03-28 15:59:52 +0100

    Add default constructor, asignment operators
    + Fixes

--- cpp_features/smart_ptrs/my_shared_ptr/my_shared_ptr.h
+++ cpp_features/smart_ptrs/my_shared_ptr/my_shared_ptr.h
@@ -3,31 +3,54 @@
 template <class T>
 class my_shared_ptr {
 
-    T* _ptr;
-    long* __shr_cntr;
+    T* _ptr = nullptr;
+    long* __shr_cntr = nullptr;
 
 public:
+
+    my_shared_ptr() = default;
+
     my_shared_ptr(T* ptr) : _ptr(ptr), __shr_cntr(new long(1)){
-        std::cout << "CONST def" << std::endl;
+        std::cout << "CONST prm" << std::endl;
     }
 
-    my_shared_ptr(my_shared_ptr& ptr) {
+    my_shared_ptr(my_shared_ptr& other) : _ptr(other._ptr), __shr_cntr(other.__shr_cntr) {
         std::cout << "CONST copy" << std::endl;
-        _ptr = ptr._ptr;
-        __shr_cntr = ptr.__shr_cntr;
-        (*__shr_cntr)++;
+
+        if (__shr_cntr) {
+            (*__shr_cntr)++;
+        }
     }
 
     my_shared_ptr(my_shared_ptr&& other) {
         std::cout << "CONST move" << std::endl;
-        _ptr = other._ptr;
-        other._ptr = nullptr;
-
-        __shr_cntr = other.__shr_cntr;
-        other.__shr_cntr = nullptr;
+        std::swap(_ptr, other._ptr);
+        std::swap(__shr_cntr, other.__shr_cntr);
     }
 
-    ~my_shared_ptr() {
+    my_shared_ptr& operator=(my_shared_ptr& other) {
+        std::cout << "ASSIGN copy" << std::endl;
+
+        _ptr = other._ptr;
+        __shr_cntr = other.__shr_cntr;
+
+        if (__shr_cntr) {
+            (*__shr_cntr)++;
+        }
+
+        return *this;
+    }
+
+    my_shared_ptr& operator=(my_shared_ptr&& other) {
+        std::cout << "ASSIGN move" << std::endl;
+
+        std::swap(_ptr, other._ptr);
+        std::swap(__shr_cntr, other.__shr_cntr);
+
+        return *this;
+    }
+
+    virtual ~my_shared_ptr() {
 
         if (__shr_cntr) {
             --(*__shr_cntr);
@@ -42,7 +65,7 @@ public:
     T& operator* () { return *_ptr; }
     T* operator-> () { return _ptr; }
 
-    long use_count() const { return *__shr_cntr; }
+    long use_count() const { return __shr_cntr ? *__shr_cntr : 0; }
 
     operator bool() const {
         return _ptr != nullptr;
--- cpp_features/smart_ptrs/my_shared_ptr/my_shared_ptr_demo.cpp
+++ cpp_features/smart_ptrs/my_shared_ptr/my_shared_ptr_demo.cpp
@@ -44,7 +44,10 @@ int main()
     assert(*sp4 == val && "Error: unexpected value inside of pointer");
     assert(sp4.use_count() == 3 && "Error: use count not equal to 3");
 
-    my_shared_ptr<int> sp5 = std::move(sp4);
+    my_shared_ptr<int> sp5;
+    sp5 = std::move(sp4);
+    sp4 = std::move(sp5);
+    sp5 = std::move(sp4);
 
     assert(!sp4 && "Error: pointer should be null");
 
@@ -55,7 +58,34 @@ int main()
     auto sp10 = make_my_shared_ptr<A>();
     sp10->a = 13;
 
-    my_shared_ptr<A> sp12(std::move(sp10));
+    my_shared_ptr<A> sp11(std::move(sp10));
+    assert(sp11.use_count() == 1 && "Error: use count not equal to 0");
+    my_shared_ptr<A> sp12 = sp11;
+    assert(sp12.use_count() == 2 && "Error: use count not equal to 0");
+
+    my_shared_ptr<A> empty_ptr;
+    empty_ptr.use_count();
+    assert(empty_ptr.use_count() == 0 && "Error: use count not equal to 0");
+
+    my_shared_ptr<A> empty_ptr2(empty_ptr);
+    empty_ptr2.use_count();
+    assert(empty_ptr2.use_count() == 0 && "Error: use count not equal to 0");
+
+    my_shared_ptr<A> empty_ptr3(empty_ptr);
+    empty_ptr3.use_count();
+    assert(empty_ptr3.use_count() == 0 && "Error: use count not equal to 0");
+
+    std::cout << "Assign" << std::endl;
+    my_shared_ptr<A> empty_ptr4;
+    empty_ptr4 = empty_ptr3;
+    empty_ptr3.use_count();
+    assert(empty_ptr3.use_count() == 0 && "Error: use count not equal to 0");
+
+    std::cout << "Assign" << std::endl;
+    my_shared_ptr<A> empty_ptr5;
+    empty_ptr5 = std::move(empty_ptr4);
+    empty_ptr4.use_count();
+    assert(empty_ptr5.use_count() == 0 && "Error: use count not equal to 0");
 
     return 0;
 }
